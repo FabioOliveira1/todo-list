be4fe990721bf522c2ff3b7d51f3ce9fabe1ae3f
FabioOliveira1 <fabio.oliveira@jquant.com.br>
2017-05-11 16:54:08 -0300

    Add simple requireAuth

--- imports/startup/client/routes.js
+++ imports/startup/client/routes.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { BrowserRouter as Router, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Redirect} from 'react-router-dom';
 import createBrowserHistory from 'history/createBrowserHistory'
 
 import  AppBody  from '../../ui/layouts/AppBody.js';
@@ -8,15 +8,20 @@ import  Dashboard  from '../../ui/layouts/Dashboard.js';
 import  Login  from '../../ui/layouts/Login.js';
 
 const history = createBrowserHistory();
+const requireAuth = (currentState) => {
+	console.log(currentState);
+	return (false) ? (<Redirect to="/main" />) : (<Redirect to="/login" />)
+}
 
 // Set up all routes in the app
 Meteor.startup(() => {
 	render(
 		<Router history={history}>
 			<div id="routes-container">
-				<Route exact path="/" component={ AppBody }/>
-				<Route path="/dashboard" component={Dashboard} />
-				<Route path="/login" component={Login} />
+				<Route exact path="/" render={requireAuth} />
+				<Route path="/main" render={requireAuth} />
+				<Route path="/dashboard"  render={requireAuth}  />
+				<Route path="/login" component={Login}  />
 			</div>
 		</Router>,
 		document.getElementById('app-container')
